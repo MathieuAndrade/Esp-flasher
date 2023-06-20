e5345be814efcd752dcb9fe9e7b0cccf5d32cc7c
Mathieu Andrade <Mathieu.botelhoandrade@gmail.com>
2023-06-20 22:56:46 +0200

    Back: Add possibility to resize window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use serialport;
+use std::{thread, time};
 use tauri::api::dialog::FileDialogBuilder;
-use tauri::Window;
+use tauri::{LogicalSize, Size, Window};
 
 pub mod progress;
 
@@ -50,13 +51,71 @@ fn flash_image(window: Window, port: String, file: String) {
     flash(window, port, 3145728, 921600, file);
 }
 
+#[tauri::command]
+fn set_window_size(window: Window, target_width: f64, target_height: f64, operation_count: f64) {
+    // Get the current window size
+    let mut current_width: f64 = window.inner_size().unwrap().width as f64;
+    let mut current_height: f64 = window.inner_size().unwrap().height as f64;
+
+    // Calculate the interval for each operation
+    // this is useful to prevent the window from resizing too fast/slow
+    // and to much cpu usage
+    let width_interval: f64;
+    let height_interval: f64;
+
+    if current_width < target_width {
+        width_interval = (target_width - current_width) / operation_count;
+    } else {
+        width_interval = (current_width - target_width) / operation_count;
+    }
+
+    if current_height < target_height {
+        height_interval = (target_height - current_height) / operation_count;
+    } else {
+        height_interval = (current_height - target_height) / operation_count;
+    }
+
+    // Spawn a thread to resize the window
+    thread::spawn(move || {
+        while current_width != target_width || current_height != target_height {
+            // Check what operation is needed to be done on the window width
+            // and set the new width of the window
+            if current_width < target_width {
+                current_width = current_width + width_interval;
+            } else if current_width > target_width {
+                current_width = current_width - width_interval;
+            }
+
+            // Check what operation is needed to be done on the window height
+            // and set the new height of the window
+            if current_height < target_height {
+                current_height = current_height + height_interval;
+            } else if current_height > target_height {
+                current_height = current_height - height_interval;
+            }
+
+            // Set the new size of the window
+            window
+                .set_size(Size::Logical(LogicalSize {
+                    width: current_width,
+                    height: current_height,
+                }))
+                .unwrap();
+
+            // Sleep for 20ms to prevent the thread from using too much CPU
+            thread::sleep(time::Duration::from_millis(20));
+        }
+    });
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             list_ports,
             flash_firmware,
             flash_image,
-            open_file_dialog
+            open_file_dialog,
+            set_window_size
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
