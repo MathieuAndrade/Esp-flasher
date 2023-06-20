34b64c58c2bfc9cb19814e0101893ca4802388da
Mathieu Andrade <Mathieu.botelhoandrade@gmail.com>
2023-06-20 22:57:29 +0200

    Front: Add Advanced options section and fix some ui issues

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,26 @@
 <script lang="ts">
-  import { Button } from "flowbite-svelte";
+  import { Accordion, Button } from "flowbite-svelte";
 
+  import AdvancedOptions from "./lib/AdvancedOptions.svelte";
   import Footer from "./lib/Footer.svelte";
-  import Titlebar from "./lib/Titlebar.svelte";
   import Options from "./lib/Options.svelte";
   import Progress from "./lib/Progress.svelte";
+  import Titlebar from "./lib/Titlebar.svelte";
 
   let flash;
 </script>
 
-<main class="flex flex-col h-full bg-gray-200 rounded-lg">
+<main class="flex flex-col h-full bg-gray-200 rounded-lg overflow-hidden">
   <Titlebar />
 
   <Options bind:flash />
 
+  <Accordion flush class="flex-grow">
+    <AdvancedOptions />
+  </Accordion>
+
   <div class="p-5 text-end">
-    <Button
-      pill={true}
-      class="!px-2 h-[46px] w-[46px]"
-      color="blue"
-      on:click={flash}
-    >
+    <Button pill={true} class="!px-2 h-[46px] w-[46px]" on:click={flash}>
       Start
     </Button>
   </div>
---
+++ src/lib/AdvancedOptions.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
+  import { AccordionItem, Input, Label, Select } from "flowbite-svelte";
+  import { afterUpdate } from "svelte";
+
+  let selectedBaudrate = "921600";
+  let optionsOpen = true;
+
+  afterUpdate(() => {
+    const button = document.querySelector("h2 button");
+
+    if (button) {
+      button.addEventListener("click", () => {
+        optionsOpen = !optionsOpen;
+
+        // Resize the window to fit the options
+        invoke("set_window_size", {
+          targetWidth: 400,
+          targetHeight: optionsOpen ? 680 : 530,
+          operationCount: 8,
+        });
+      });
+    }
+  });
+</script>
+
+<AccordionItem
+  defaultClass="w-[-webkit-fill-available] flex items-center justify-between font-medium text-left border-b border-gray-400/70"
+  paddingFlush="mx-5 py-2"
+  open
+>
+  <span slot="header"> Advanced options </span>
+
+  <div slot="arrowup">
+    <svg
+      class="w-6 h-6 shrink-0 rotate-180"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M7.4 15.4L6 14l6-6l6 6l-1.4 1.4l-4.6-4.6l-4.6 4.6Z" />
+    </svg>
+  </div>
+  <div slot="arrowdown">
+    <svg
+      class="w-6 h-6 shrink-0"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M7.4 15.4L6 14l6-6l6 6l-1.4 1.4l-4.6-4.6l-4.6 4.6Z" />
+    </svg>
+  </div>
+
+  <div class="flex gap-3">
+    <Label class="block space-y-2">
+      <span>Baudrate</span>
+      <Select
+        class="cursor-pointer"
+        size="sm"
+        items={[
+          { value: "921600", name: "921600" },
+          { value: "57600", name: "57600" },
+          { value: "256000", name: "256000" },
+          { value: "512000", name: "512000" },
+        ]}
+        bind:value={selectedBaudrate}
+      />
+    </Label>
+    <div>
+      <Label for="flashAddress" class="mb-2">Flash Address</Label>
+      <Input type="text" id="flashAddress" placeholder="0x0" class="h-[38px]" />
+    </div>
+  </div>
+</AccordionItem>
